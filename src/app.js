6f0964d5a5defc2d221282a9bb9f6df812f71f46
Adam <adborowski@gmail.com>
2020-03-22 13:14:21 +0100

    Completed the setup for styling, added a CSS reset (normalize), did basic header styles

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ console.log("[APP.JS]");
 import ReactDOM from 'react-dom';
 import React from 'react';
 import IndecisionApp from './components/IndecisionApp.js'
+import '../node_modules/normalize.css/normalize.css' // css reset
 import './styles/styles.scss';
 
 const appRoot = document.getElementById("app");
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@
 // stateless functional component (faster than class-based)
 const Header = (props) => (
     
-        <div>
-            <h1>{props.title}</h1>
-            <h2>{props.subtitle}</h2>
+        <div className="header">
+            <h1 className="header__title">{props.title}</h1>
+            <h2 className="header__subtitle">{props.subtitle}</h2>
         </div>
     
 )
