6e0a568625fe97cb7fa9501fe57e40037899ffb0
Adam <adborowski@gmail.com>
2020-04-05 11:52:05 +0200

    Began exploring Redux in isolation

---
+++ src/playground/expensify-1.js
@@ -0,0 +1,14 @@
+﻿console.log("[APP.JS]");
+
+import ReactDOM from 'react-dom';
+import React from 'react';
+import '../node_modules/normalize.css/normalize.css' // css reset
+import './styles/styles.scss';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
+
+
+
+const appRoot = document.getElementById("app");
+
+ReactDOM.render(<AppRouter/>, appRoot); // you can straight up stick your component JSX tag in there
---
+++ src/playground/redux-101.js
@@ -0,0 +1,59 @@
+﻿// console.log("[REDUX-101.JS] Remember the changed entry value in webpack config");
+
+import {createStore} from 'redux';
+
+const store = createStore( (state = {count:0}, action) => {
+
+    switch (action.type){
+
+        case "INCREMENT": 
+
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
+            return {
+                count: state.count+incrementBy
+            }
+
+        case "DECREMENT":
+
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy: 1
+            return {
+                count: state.count-decrementBy
+            }
+
+        case "RESET":
+            return {
+                count: 0
+            }
+
+        default:
+            return state
+
+    }
+});
+
+const unsubscribe = store.subscribe(()=>{
+    console.log(store.getState());
+});
+
+// unsubscribe();
+
+// let's increment the count
+store.dispatch({
+    type: "INCREMENT",
+    incrementBy: 5
+});
+
+// let's increment the count
+store.dispatch({
+    type: "INCREMENT"
+});
+
+// let's reset
+store.dispatch({
+    type: "RESET"
+});
+
+// let's decrement the count
+store.dispatch({
+    type: "DECREMENT"
+});
