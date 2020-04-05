deb22c7ea3ca1f204ea8ff852958d87bb2314f0c
Adam <adborowski@gmail.com>
2020-04-05 19:52:22 +0200

    Finished learning about Object and Array Destructuring

---
+++ src/playground/destructuring-1.js
@@ -0,0 +1,47 @@
+﻿console.log("[DESTRUCTURING.JS]");
+
+const person = {
+    name: "Adam",
+    age: 26,
+    location: {
+        city: "Copenhagen",
+        temp: "10C"
+    }
+}
+
+// this is the basic way
+console.log(`${person.name} is ${person.age} years old.`);
+
+// we can simplify variable names like this (commenting out because of dupe declaration error)
+// const name = person.name;
+// const age = person.age;
+// console.log(`${name} is ${age} years old.`);
+
+// we can simplify even more by using object destructuring
+const {name, age} = person
+console.log(`${name} is ${age} years old.`)
+
+// you can enter deeper object layers like this
+const {city, temp} = person.location;
+console.log(`It's currently ${temp} in ${city}`);
+
+// you can also give the destructured variables new names
+const {city: town, temp: temperature} = person.location;
+console.log(`It's currently ${temperature} in ${town}`)
+
+// you can also add default values
+const {country: nation = "Default Country"} = person.location;
+console.log(`${name} comes from ${nation}`)
+
+// now a challenge from Andrew Mead
+
+const book = {
+    title: 'Ego is the enemy',
+    author: 'Ryan Holiday',
+    publisher: {
+        name: 'Penguin'
+    }
+}
+
+const {name : publisherName = 'Self-Published'} = book.publisher; // change name to publisherName and set a default of 'Self-Published' if undefined
+console.log(publisherName); // use Penguin if available, or the default of Self-Published if not
---
+++ src/playground/destructuring-2.js
@@ -0,0 +1,23 @@
+﻿// and now for array destructuring
+
+const address = ['Husumgade 44A', 'Copenhagen', 'Denmark', '2200']; // you can get this kind of data
+console.log(`You're in ${address[1]}, ${address[2]}`); // the easy but ugly way - does not scale well
+
+// for destructuring objects, we use {} and for destructuring arrays we use []
+
+const [street, city, country, postcode] = address; // name the variables in the same order they come up in the address array
+console.log(`You're in ${city}, ${country}`); // this is the destructured method and scales very well
+
+// you don't have to name all the variables. You can skip some. Just have empty spaces between the destructuring brackets, but leave the commas.
+// you can also set defaults, even for non-existent new destructured variables, like continent
+
+const [, town, , , continent = "Default continent"] = address;
+console.log(`You're in ${town}, ${continent}`);
+
+// and now a challenge
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$3.00'];
+const [itemName, , priceMedium] = item;
+
+console.log(`A medium ${itemName} costs ${priceMedium}`);
+
